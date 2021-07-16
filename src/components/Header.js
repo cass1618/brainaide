46e0776d0cf6463b6c9db229b4c86502f405850f
cass1618 <cass16180@gmail.com>
2021-07-16 13:28:12 -0700

    :building_construction: Add header

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+    return (
+        <React.Fragment>
+            <h1>BRAINAIDE</h1>
+            <h2>Currently in development. Expected completion July 28.</h2>
+        </React.Fragment>
+    );
+}
+
+export default Header;
