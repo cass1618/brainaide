9c05972e3ba9e9d7d10db5591bbafdfc97b9b1f4
cass1618 <cass16180@gmail.com>
2021-07-16 14:02:25 -0700

    :clipboard: Add form for URL upload

---
+++ src/components/UrlForm.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function UrlForm(props) {
+    return (
+        <React.Fragment>
+            <form onSubmit={handleUrlFormSubmission}>
+                <input
+                    type="url"
+                    name="urlname"
+                    placeholder="Enter URL"/>
+            </form>
+        </React.Fragment>
+    );
+}
+
+export default NewTicketForm;
