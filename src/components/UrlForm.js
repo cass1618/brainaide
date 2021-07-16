58a40078446b5b9ef5c028595066e5e520ca0cad
cass1618 <cass16180@gmail.com>
2021-07-16 15:38:12 -0700

    :abc: Add text section object component

--- src/components/UrlForm.js
+++ src/components/UrlForm.js
@@ -5,9 +5,10 @@ function UrlForm(props) {
 
     function handleUrlFormSubmission(e) {
         e.preventDefault();
+        console.log("FORM");
         props.onLoadingUrl(console.log("LOADING URL"));
     }
-    
+
     return (
         <React.Fragment>
             <form onSubmit={handleUrlFormSubmission}>
