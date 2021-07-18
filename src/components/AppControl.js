9d000d65eae812a2590b07254725ee675f52812a
cass1618 <cass16180@gmail.com>
2021-07-17 17:55:52 -0700

    :computer: HTML is rendered to display the source webpage

--- src/components/AppControl.js
+++ src/components/AppControl.js
@@ -5,6 +5,7 @@ import PropTypes from "prop-types";
 import * as a from './../actions';
 // import DisplayText from "./DisplayText";
 import request from "request-promise";
+import {Markup} from "interweave";
 
 class AppControl extends React.Component {
 
@@ -13,54 +14,20 @@ class AppControl extends React.Component {
         this.state = {
             error: null,
             isLoaded: false,
-            headlines: "text"
+            rawHtml: "empty"
         };
     };
 
-    // makeApiCall = () => {
-    //     fetch(`http://api.scraperapi.com?api_key=${process.env.REACT_APP_API_KEY}&url=https://www.learnhowtoprogram.com/react&render=true&autoparse=true&country_code=us`)
- 
-    //         .then(response => response.json())
-          
-    //         .then(body => {
-    //             console.log(body);
-    //         })
-            
-    //             .catch((error) => {
-    //                 this.setState({
-    //                     isLoaded: true,
-    //                     error
-    //                 });
-    //             });
-    // }
-
     makeApiCall = () => {
-        //fetch(`https://api.nytimes.com/svc/topstories/v2/home.json?api-key=${process.env.REACT_APP_API_KEY}`)
-        // fetch(`http://api.scraperapi.com?api_key=${process.env.REACT_APP_API_KEY}&url=https://www.learnhowtoprogram.com/react&render=true&autoparse=true&country_code=us`)
-        //   .then(response => response.json())
-        //   .then(
-        //     (jsonifiedResponse) => {
-        //       this.setState({
-        //         isLoaded: true,
-        //         headlines: jsonifiedResponse.results
-        //       });
-        //     })
-        //     .catch((error) => {
-        //       this.setState({
-        //         isLoaded: true,
-        //         error
-        //       });
-        //     });
-        //const request = require('request-promise');
+ 
+        request(`http://api.scraperapi.com?api_key=${process.env.REACT_APP_API_KEY}&url=https://www.learnhowtoprogram.com/react/functional-programming-with-javascript/using-node-in-the-command-line&render=true&autoparse=true&country_code=us`)
 
-        request(`http://api.scraperapi.com?api_key=${process.env.REACT_APP_API_KEY}&url=https://www.learnhowtoprogram.com/react&render=true&autoparse=true&country_code=us`)
-        // .then(response => console.log(response))
         .then(response => {
             this.setState({
                 isLoaded: true,
-                headlines: response
+                rawHtml: response
             });
-        }).then((headlines)=> console.log(headlines))
+        })
         .catch(error => {
         console.log(error)
         })
@@ -80,64 +47,63 @@ class AppControl extends React.Component {
 
     //     let currentlyVisibleState = null;
 
-    //     const {error, isLoaded, rawhtml} = this.state;
+        // const {error, isLoaded, rawhtml} = this.state;
 
-    //     if(error) {
-    //         return (
-    //             <React.Fragment>
-    //                 ERROR: {error.message}
-    //             </React.Fragment>
-    //         )
-    //     } else if (!isLoaded) {
-    //         return (
-    //             <React.Fragment>
-    //                 LOADING . . .
-    //             </React.Fragment>
-    //             )
-    //     } else {
-    //         return (
-    //             <React.Fragment>
-    //                 <h1>RAW HTML</h1>
-    //                 <p>{rawhtml}</p>
-    //             </React.Fragment>
-    //         )
-    //     }
+        // if(error) {
+        //     return (
+        //         <React.Fragment>
+        //             ERROR: {error.message}
+        //         </React.Fragment>
+        //     )
+        // } else if (!isLoaded) {
+        //     return (
+        //         <React.Fragment>
+        //             LOADING . . .
+        //         </React.Fragment>
+        //         )
+        // } else {
+        //     return (
+        //         <React.Fragment>
+        //             <h1>RAW HTML</h1>
+        //             <p>{rawhtml}</p>
+        //         </React.Fragment>
+        //     )
+        // }
 
-    //     if(this.props.formVisibleOnPage) {
+        // if(this.props.formVisibleOnPage) {
 
-    //     } else {
-    //         currentlyVisibleState = 
-    //             <DisplayText
-    //                 displayText = {this.props.allSections}/>
-    //     }
+        // } else {
+        //     currentlyVisibleState = 
+        //         <DisplayText
+        //             displayText = {this.props.allSections}/>
+        // }
         
-    //     return (
+        // return (
 
-    //         <React.Fragment>
-    //             <UrlForm
-    //                 onLoadingUrl = {this.handleLoadingUrl}/>
-    //             {currentlyVisibleState}
-    //             {/* <button onClick = {this.handleClick}>BUTTON</button> */}
-    //         </React.Fragment>
-    //     )
+        //     // <React.Fragment>
+        //     //     <UrlForm
+        //     //         onLoadingUrl = {this.handleLoadingUrl}/>
+        //     //     {currentlyVisibleState}
+        //     //     {/* <button onClick = {this.handleClick}>BUTTON</button> */}
+        //     // </React.Fragment>
+    //     // )
     // }
 
     render() {
-        const { error, isLoaded, headlines } = this.state;
+        const { error, isLoaded, rawHtml } = this.state;
         if (error) {
-          return <React.Fragment>Error: {error.message}</React.Fragment>;
+            return <React.Fragment>Error: {error.message}</React.Fragment>;
         } else if (!isLoaded) {
-          return <React.Fragment>Loading...</React.Fragment>;
+            return <React.Fragment>Loading...</React.Fragment>;
         } else {
-          return (
-            <React.Fragment>
-              <h1>Headlines</h1>
-              <ul>
-                {headlines
-                }
-              </ul>
-            </React.Fragment>
-          );
+            const body = rawHtml.split("<body>")[1]
+            console.log(body);
+            return (
+                <React.Fragment>
+                <h1>rawHtml</h1>
+                        <Markup content={body}/>
+                </React.Fragment>
+            );
         }
       }
 
