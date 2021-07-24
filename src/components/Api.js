b099a2c27d5d87fea783f91a20b9e6d8292055f8
cass1618 <cass16180@gmail.com>
2021-07-24 00:55:16 -0700

    :star: Successfully call api with user inputted url

--- src/components/Api.js
+++ src/components/Api.js
@@ -5,12 +5,16 @@ function Api(props) {
     function makeApiCall(e) { 
         e.preventDefault();
 
-        props.makeApiCall();
+        props.makeApiCall(e.target.url.value);
     }
 
     return (
         <React.Fragment>
             <form onSubmit={makeApiCall}>
+                <input
+                    type = "url"
+                    name = "url"
+                    placeholder = "Enter url"/>
                 <button type="submit">CALL API</button>
             </form>
         </React.Fragment>
